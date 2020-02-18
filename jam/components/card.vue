7b394e6f3b9e98e4dbfb4b3fc67a1aef4192b1cf
Momiji" Michitsugu Sakamoto <m_sakamoto@nanika-e-idea.com>
2020-02-18 07:50:44 +0000

    build nuxtjs

---
+++ jam/components/card.vue
@@ -0,0 +1,9 @@
+<template>
+  <article class="card">
+    <nuxt-link :to="{ name: 'blog-slug'}" class="wrapper">
+      <h1 class="card_title">記事１</h1>
+      <p class="card_text">記事の内容　hogehogehogehoge....</p>
+      <p class="card_date">2020/2/18</p>
+    </nuxt-link>
+  </article>
+</template>
---
+++ jam/pages/blog/_slug.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="slug">
+    <h1 class="slug_title">タイトル</h1>
+    <p class="slug_date">2020/2/18</p>
+    <div>
+      記事の内容　hogehogehogehogehoge....
+    </div>
+  </section>
+</template>
